e74932a2187eb7888324ee1280f623f93103246f
Melissa Hsiung <melissa@mhsiung.com>
2023-08-18 09:18:02 -0400

    Update blog.php
    Removed duplicate code (Thanks Alja!)

--- inc/blog.php
+++ inc/blog.php
@@ -18,26 +18,6 @@ function cat_output_blog_footer() {
 	endif;
 }
 
-<?php
-/**
- * Customisations to the blog posts
- */
-
-add_action( 'neve_after_post_content', 'cat_output_blog_footer' );
-
-/**
- * Output widget area at the bottom of a single blog post.
- */
-function cat_output_blog_footer() {
-	if ( is_active_sidebar( 'cat-blog-footer' ) && is_singular( 'post' ) ) :
-
-		echo '<div class="widget-area" role="complementary">';
-		dynamic_sidebar( 'cat-blog-footer' );
-		echo '</div>';
-
-	endif;
-}
-
 /**
  * Add coauthors (Coauthors Plus plugin must be activated)
  */
